1ef9ef3de3f19e5eabaebe29d479ffa28bcc8088
PornthepChanpheng <pornthepuru@gmail.com>
2023-11-09 12:57:07 +0700

    add readme.md

---
+++ array.py
@@ -0,0 +1,18 @@
+from array import *
+
+array1 = array('i', [10,20,30,40,50])
+
+array1.insert(1,60)
+
+array1.remove(40)
+
+print(array1.index(30))
+
+array1[2] = 80
+
+for x in array1:
+   print(x)
+
+print(array1[0])
+print(array1[2])
+
